557c27598cffe2c5f2e07092ac86f197591fb6d0
codedeedope <26957536+codedeedope@users.noreply.github.com>
2021-10-27 18:51:23 +0200

    build urcl

--- UniversalRobotsClientLibrary/build_Linux64.sh
+++ UniversalRobotsClientLibrary/build_Linux64.sh
@@ -7,14 +7,18 @@ cd "$scriptDir"
 source "./_bash_config.sh"
 
 run() {
-    cd ./Universal_Robots_Client_Library
-    rm -rdf build
-    mkdir build
-    cd build
-    cmake ..
-    make
-    #--jobs="$((2*$(nproc)))"
+    local -r currentBuild="$currentTmp/build"
+    rm -rdf $currentBuild
+    mkdir $currentBuild
+    
+    local -r currentInstall="$(realpath "$currentTarget")"
+    rm -rdf $currentInstall
+    mkdir $currentInstall
 
+    cd $currentBuild
+    cmake -DCMAKE_INSTALL_PREFIX="$currentInstall" $scriptDir/Universal_Robots_Client_Library
+    make --jobs="$((2*$(nproc)))"
+    make install
     cd "$scriptDir"
 }
 
