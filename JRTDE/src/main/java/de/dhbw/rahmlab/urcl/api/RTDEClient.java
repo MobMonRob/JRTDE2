737c739c9909bc5fb9f9bdfa9373eb70c3de95eb
Oliver Rettig <Oliver.Rettig@orat.de>
2022-01-12 15:10:53 +0100

    javadoc changes only

--- JRTDE/src/main/java/de/dhbw/rahmlab/urcl/api/RTDEClient.java
+++ JRTDE/src/main/java/de/dhbw/rahmlab/urcl/api/RTDEClient.java
@@ -28,16 +28,17 @@ public class RTDEClient {
     /**
      * Reads the pipeline to fetch the next data package (blocking api).<br>
      * <br>
+     * Read latest RTDE package. This will block for timeout_in_ms, so the
+     * robot will effectively be in charge of setting the frequency of this loop unless RTDE
+     * communication doesn't work in which case the user will be notified.
+     * In a real-world application this thread should be scheduled with real-time priority in order
+     * to ensure that this is called in time.
+     * <br>
      * @param timeout_in_ms Time to wait if no data package is currently in the queue<br>
      * <br>
      * @return Unique ptr to the package, if a package was fetched successfully, null otherwise
      */
     public RobotState receiveData(long timeout_in_ms){
-        // Read latest RTDE package. This will block for timeout_in_ms, so the
-        // robot will effectively be in charge of setting the frequency of this loop unless RTDE
-        // communication doesn't work in which case the user will be notified.
-        // In a real-world application this thread should be scheduled with real-time priority in order
-        // to ensure that this is called in time.
         DataPackage result = client.getDataPackage(timeout_in_ms);
         if (result != null){
             return new RobotState(result);
